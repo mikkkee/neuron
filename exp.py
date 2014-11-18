ecd3409ee9ef11f1fb8cb5ff30e0d1ad4652b876
Mikkkee <boy.swordman@gmail.com>
2014-11-18 22:18:43 +0800

    update exp

--- exp.py
+++ exp.py
@@ -1,4 +1,6 @@
 import random
+import math
+import argparse
 
 from neuron import Node, Path, Neuron
 from neuron import coor_equal, check_connections, stats_connections
@@ -29,37 +31,99 @@ def pattern42(N, nx, ny, p=0.3):
     return neurons
 
 
+def pattern6(nx, ny, p=0.3):
+    '''Pattern 6.'''
+
+    ny1 = ny / 2
+    ny2 = ny - ny1
+
+    grid = []
+    lattice = settings.MAX_PATH_LENGTH
+    h = math.sqrt(3)
+
+    n_neurons = int(nx*ny*p)
+
+    for i in range(ny1):
+        y = h*i
+        for j in range(nx):
+            x = j - 0.5
+            grid.append((x*lattice, y*lattice))
+
+    for i in range(ny2):
+        y = h / 2 + i * h
+        for j in range(nx):
+            x = j
+            grid.append((x*lattice, y*lattice))
+            
+
+    node_coors = random.sample(grid, n_neurons)
+
+    neurons = [Neuron(Node(x, 6)) for x in node_coors]
+
+    return neurons
+
+
 def main():
 
-    N = 4
+    # Number of pattern edges
+    N = 2
+    # Number of runs.
+    N_run = 3
+    # Data file
+    out_file_name = 't_percentage_{n}.dat'.format(n=N)
     nx = 18
     ny = 14
     # Number of timesteps
-    T = 120
+    T = 9
 
-    neurons = pattern42(N, nx, ny)
+    data = {}
+    for r in range(N_run):
+        # Set up neurons.
+        if N != 6:
+            neurons = pattern42(N, nx, ny)
+        else:
+            neurons = pattern6(nx, ny)
 
-    for item in neurons:
-        item.born()
-
-    t = 0
-    while t<=T:
-        # Grow and clean for each neuron.
         for item in neurons:
-            item.grow()
-            item.clean()
+            item.born()
 
-        # Record connected neurons.
-        connected = [x for x in neurons if x.connected]
+        # Reset timestep and percentage.
+        t = 0
+        percentage = 0
 
-        # Check connections
-        check_connections(neurons, connected)
+        # Run for T steps.
+        while t<=T:
+            # Grow and clean for each neuron.
+            for item in neurons:
+                item.grow()
+                item.clean()
 
-        # Stats connections
-        print stats_connections(neurons)
+            # Record connected neurons.
+            connected = [x for x in neurons if x.connected]
 
-        # Increase timestep
-        t += 1
+            # Check connections
+            check_connections(neurons, connected)
+
+            # Percentage for previous step.
+            print t+1, percentage
+            if t not in data.keys():
+                data[t] = [percentage]
+            else:
+                data[t].append(percentage)
+
+            # Stats connections after updated.
+            percentage = stats_connections(neurons)
+
+            # Increase timestep
+            t += 1
+
+    # Write data
+    with open(out_file_name, 'w') as data_file:
+        time_series = sorted(data.keys())
+        for t in time_series:
+            data_file.write('\n{tt} '.format(tt=t))
+            for p in data[t]:
+                data_file.write('{pp:.3f} '.format(pp=p))
 
 if __name__ == '__main__':
     main()
