e81791fdfa7beebe9c18ada55340707506c46a78
Mikkkee <boy.swordman@gmail.com>
2014-12-14 22:58:24 +0800

    init settings

---
+++ flat/settings.py
@@ -0,0 +1,32 @@
+# Expected number of branching events at an isolated segment with infinite
+# period of time - B.
+BRANCH_COUNT_INFINITE = 5
+
+# Exponetial decay parameter in baseline branching rate - tau.
+BRANCH_DECAY_TAU = 1
+
+# Branching competetion parameter between tip nodes - E.
+BRANCH_COMPETETION_E = 1
+
+# Parameter determine influence of segments with different heights.
+TOPOLOGICAL_PARA_S = 1
+
+# Minimal and maximumal rotation angle.
+ALPHA_MIN = 0.0
+ALPHA_MAX = 0.0
+
+# Average elongation velocity.
+# TODO: Add required units.
+AVE_VELOCITY = 5
+
+# Initial length for children.
+# TODO: Add required units.
+INIT_LEN = 5.0
+
+# Timestep length.
+# TODO: Add required units.
+TIMESTEP = 5.0
+
+# Turns rate. How many turns a neurite makes during one unit of time.
+# unit: turns/length.
+TURNS_RATE = 5
